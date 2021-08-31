134bb83d765544711f517ca67d8e1099b1f663bc
James Simpson <edenofest@gmail.com>
2021-08-31 12:22:36 -0400

    feat(nix): Add nix flake env

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Rust Chip-8 emulator";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, utils, ... }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+      in
+        {
+          devShell = pkgs.mkShell {
+            name = "chippy";
+            buildInputs = with pkgs; [
+              rust
+              rust-analyzer
+              cargo-watch
+            ];
+
+            RUST_BACKTRACE = 1;
+          };
+        }
+    );
+}
